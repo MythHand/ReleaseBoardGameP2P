a90eac08d1c10553505379389b2e1a0a4173bf49
ditayler <ditayler@icloud.com>
2026-08-14 21:14:30 +0200

    feat(web): a batch becomes beats in the order it happened (#97)

--- apps/frontend/src/features/board-beats/index.ts
+++ apps/frontend/src/features/board-beats/index.ts
@@ -1,4 +1,4 @@
-export type { BeatPlan, DiscardCard, DiscardSource } from './planBeats'
-export { planBeats } from './planBeats'
+export type { BeatPlan, DiscardCard, DiscardSource, PileStep, PlannedDraw } from './planBeats'
+export { classifyPiles, planBeats } from './planBeats'
 export type { Beats } from './useBeats'
 export { useBeats } from './useBeats'
